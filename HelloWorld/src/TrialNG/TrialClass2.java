29c911572c443cba1789147b19f75ffc021cae1c
ssrivastav <ssrivastav@SSRIVASTAVLT2.tbccorp.com>
2020-01-21 16:12:54 -0500

    5

--- HelloWorld/src/TrialNG/TrialClass2.java
+++ HelloWorld/src/TrialNG/TrialClass2.java
@@ -3,8 +3,9 @@ package TrialNG;
 import org.testng.annotations.Test;
 
 public class TrialClass2 {
+	String x="Whats up";
   @Test
   public void Earth() {
-	  System.out.println("Hello Earth");
+	  System.out.println(x);
   }
 }
